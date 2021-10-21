b26fd38f6a43bb149b520e9cf04fc8513c855b73
Owen Ayers <54420696+OwenJAYEH@users.noreply.github.com>
2021-10-20 18:44:40 -0600

    Added Controller Support

--- Daemon Minigame/Assets/Scripts/SimpleCarController.cs
+++ Daemon Minigame/Assets/Scripts/SimpleCarController.cs
@@ -33,7 +33,7 @@ public class SimpleCarController : MonoBehaviour {
     public void GetInput()
 	{
 		m_horizontalInput = Input.GetAxis("Horizontal");
-		m_verticalInput = Input.GetAxis("Vertical");
+		m_verticalInput = Input.GetAxis("Accelerate");
 	}
 
 	private void Steer()
